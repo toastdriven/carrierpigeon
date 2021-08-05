eac15e4d359a727043d7f5bc7337415bde065116
Daniel Lindsley <daniel@toastdriven.com>
2021-08-05 05:04:16 -0500

    Reorganized the schema_creator bits.

---
+++ bin/schema_creator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import json
+
+from carrierpigeon import utils
+
+
+if __name__ == "__main__":
+    filename = input("Desired filename: ")
+
+    print()
+
+    fields = []
+    required = []
+
+    while True:
+        fieldname = input("Field name (blank to finish): ").strip()
+
+        if not fieldname:
+            break
+
+        field_type = input("Field type [string]: ").strip()
+
+        if not field_type:
+            field_type = "string"
+
+        fields.append([fieldname, field_type])
+        is_required = input(f"Is '{fieldname}' required [y/N]: ").strip()
+
+        if is_required.lower().startswith("y"):
+            required.append(fieldname)
+
+        print()
+
+    print()
+
+    schema = utils.create_schema(utils.BASE_SCHEMA, filename, fields, required)
+    print(json.dumps(schema, indent=4))
--- src/carrierpigeon/utils.py
+++ src/carrierpigeon/utils.py
@@ -1,6 +1,4 @@
-import builtins
 import copy
-import json
 
 
 BASE_SCHEMA = {
@@ -32,36 +30,3 @@ def create_schema(base, filename, fields, required=None):
     new_schema["required"].extend(required)
 
     return new_schema
-
-
-if __name__ == "__main__":
-    filename = input("Desired filename: ")
-
-    print()
-
-    fields = []
-    required = []
-
-    while True:
-        fieldname = input("Field name (blank to finish): ").strip()
-
-        if not fieldname:
-            break
-
-        field_type = input("Field type [string]: ").strip()
-
-        if not field_type:
-            field_type = "string"
-
-        fields.append([fieldname, field_type])
-        is_required = input(f"Is '{fieldname}' required [y/N]: ").strip()
-
-        if is_required.lower().startswith("y"):
-            required.append(fieldname)
-
-        print()
-
-    print()
-
-    schema = create_schema(BASE_SCHEMA, filename, fields, required)
-    print(json.dumps(schema, indent=4))
